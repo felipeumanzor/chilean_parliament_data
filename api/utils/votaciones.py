9d04ec6bb698e14e5a82c1d26e1b59d2051a5ea6
Eun Lee <cs.eunlee@gmail.com>
2019-11-07 18:37:04 -0300

    added function param

--- api/utils/votaciones.py
+++ api/utils/votaciones.py
@@ -5,10 +5,22 @@ from bson import json_util
 from django.http import HttpResponse
 
 
-def get_by_diputado():
+def get_by_diputado(diputado_id):
     proyectos = []
-    for doc in get_db().proyectosdeley.find({}, { "_id":0 ,"NumeroBoletin":1 , "Nombre":1 , "TipoIniciativa":1 , "Materias":1 }).sort('Votaciones.VotacionProyectoLey.Fecha', DESCENDING):
-        proyectos.append(doc )
+    for doc in (
+        get_db()
+        .proyectosdeley.find(
+            {},
+            {
+                "_id": diputado_id,
+                "NumeroBoletin": 1,
+                "Nombre": 1,
+                "TipoIniciativa": 1,
+                "Materias": 1,
+            },
+        )
+        .sort("Votaciones.VotacionProyectoLey.Fecha", DESCENDING)
+    ):
+        proyectos.append(doc)
     return proyectos
-
 
