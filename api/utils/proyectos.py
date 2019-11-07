dc3d92f101b484612fce06360f32f3009ea2fe88
Felipe Umanzor <umanzor.felipe@gmail.com>
2019-11-07 19:44:53 -0300

    update by_author schema

--- api/utils/proyectos.py
+++ api/utils/proyectos.py
@@ -46,6 +46,37 @@ def get_by_id(id):
 
 def get_by_author(author_id):
     proyectos = []
-    for doc in get_db().proyectosdeley.find({"Autores.ParlamentarioAutor.Diputado.Id":f'{author_id}'}, { "_id":0 ,"NumeroBoletin":1 , "Nombre":1 , "TipoIniciativa":1 , "Materias":1 }).sort('Votaciones.VotacionProyectoLey.Fecha', DESCENDING):
-        proyectos.append(doc )
+    for doc in get_db().proyectosdeley.aggregate([
+            {
+                '$match': {
+                    'Votaciones.VotacionProyectoLey.Tipo.@Valor': '1',
+                    'Autores.ParlamentarioAutor.Diputado.Id':f'{author_id}'
+                }
+            }, {
+                '$unwind': {
+                    'path': '$Votaciones.VotacionProyectoLey'
+                }
+            }, {
+                '$match': {
+                    'Votaciones.VotacionProyectoLey.TipoVotacionProyectoLey.@Valor': '1'
+                }
+            }, {
+                '$sort': {
+                    'Votaciones.VotacionProyectoLey.Fecha': -1
+                }
+            }, {
+                '$project': {
+                    '_id': 0, 
+                    'Id': 1, 
+                    'NumeroBoletin': 1, 
+                    'Nombre': 1, 
+                    'FechaIngreso': 1, 
+                    'Votaciones.VotacionProyectoLey.Id': 1, 
+                    'Votaciones.VotacionProyectoLey.Resultado': 1, 
+                    'Votaciones.VotacionProyectoLey.Fecha': 1,
+                    'Materias': 1
+                }
+            }
+        ]):
+            proyectos.append(doc )
     return proyectos
