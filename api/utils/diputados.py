dd75342946dbc996aa31982955eb5fd7b3002ea8
Eun Lee <cs.eunlee@gmail.com>
2019-11-07 19:10:47 -0300

    filter deputy true

--- api/utils/diputados.py
+++ api/utils/diputados.py
@@ -7,9 +7,38 @@ from django.http import HttpResponse
 
 def index():
     diputados = []
-    for doc in get_db().diputados.find({}, { "_id":0 ,"Id":1 , "Nombre":1 , "ApellidoPaterno":1 , "ApellidoMaterno":1, "Militancias":1, "Sexo":1 , "FechaNacimiento":1 }):
-        diputados.append(doc )
+    for doc in get_db().diputados.find(
+        {},
+        {
+            "_id": 0,
+            "Id": 1,
+            "Nombre": 1,
+            "ApellidoPaterno": 1,
+            "ApellidoMaterno": 1,
+            "Militancias": 1,
+            "Sexo": 1,
+            "FechaNacimiento": 1,
+            "Actual": True,
+        },
+    ):
+        diputados.append(doc)
     return diputados
 
+
 def get_by_id(id):
-    return list(get_db().diputados.find({ 'Id':str(id) }, { "_id":0 ,"Id":1 , "Nombre":1 , "ApellidoPaterno":1 , "ApellidoMaterno":1, "Militancias":1, "Sexo":1 , "FechaNacimiento":1, "gastos":1 }))[0]
+    return list(
+        get_db().diputados.find(
+            {"Id": str(id)},
+            {
+                "_id": 0,
+                "Id": 1,
+                "Nombre": 1,
+                "ApellidoPaterno": 1,
+                "ApellidoMaterno": 1,
+                "Militancias": 1,
+                "Sexo": 1,
+                "FechaNacimiento": 1,
+                "gastos": 1,
+            },
+        )
+    )[0]
