bb21b4c77aef8289e54f8468f66fdb5bfc2edfd3
Eun Lee <cs.eunlee@gmail.com>
2019-11-11 14:53:55 -0300

    Allowed cors

--- votoparlamentario/settings.py
+++ votoparlamentario/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ["votoparlamentario.cl", "api.votoparlamentario.cl", "127.0.0.1"
 
 INSTALLED_APPS = [
     "django.contrib.admin",
+    "corsheaders",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
@@ -42,12 +43,15 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = "votoparlamentario.urls"
 
