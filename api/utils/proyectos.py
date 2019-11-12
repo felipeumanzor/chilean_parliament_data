dc899273b9d88c57dee5e720a9d98c7e606193cd
Eun Lee <cs.eunlee@gmail.com>
2019-11-12 01:22:36 -0300

    hotfix

--- api/utils/proyectos.py
+++ api/utils/proyectos.py
@@ -7,76 +7,82 @@ from django.http import HttpResponse
 
 def index():
     proyectos = []
-    for doc in get_db().proyectosdeley.aggregate([
-        {
-            '$match': {
-                'Votaciones.VotacionProyectoLey.Tipo.@Valor': '1'
-            }
-        }, {
-            '$unwind': {
-                'path': '$Votaciones.VotacionProyectoLey'
-            }
-        }, {
-            '$match': {
-                'Votaciones.VotacionProyectoLey.TipoVotacionProyectoLey.@Valor': '1'
-            }
-        }, {
-            '$sort': {
-                'Votaciones.VotacionProyectoLey.Fecha': -1
-            }
-        }, {
-            '$project': {
-                '_id': 0, 
-                'Id': 1, 
-                'NumeroBoletin': 1, 
-                'Nombre': 1, 
-                'FechaIngreso': 1, 
-                'Votaciones.VotacionProyectoLey.Id': 1, 
-                'Votaciones.VotacionProyectoLey.Resultado': 1, 
-                'Votaciones.VotacionProyectoLey.Fecha': 1,
-                'Materias': 1
-            }
-        }
-    ]):
-        proyectos.append(doc )
+    for doc in get_db().proyectosdeley.aggregate(
+        [
+            {"$match": {"Votaciones.VotacionProyectoLey.Tipo.@Valor": "1"}},
+            {"$unwind": {"path": "$Votaciones.VotacionProyectoLey"}},
+            {
+                "$match": {
+                    "Votaciones.VotacionProyectoLey.TipoVotacionProyectoLey.@Valor": "1"
+                }
+            },
+            {"$sort": {"Votaciones.VotacionProyectoLey.Fecha": -1}},
+            {
+                "$project": {
+                    "_id": 0,
+                    "Id": 1,
+                    "NumeroBoletin": 1,
+                    "Nombre": 1,
+                    "FechaIngreso": 1,
+                    "Votaciones.VotacionProyectoLey.Id": 1,
+                    "Votaciones.VotacionProyectoLey.Resultado": 1,
+                    "Votaciones.VotacionProyectoLey.Fecha": 1,
+                    "Materias": 1,
+                }
+            },
+        ]
+    ):
+        proyectos.append(doc)
     return proyectos
 
+
 def get_by_id(id):
-    return get_db().diputados.find({}, { "_id":0 ,"Id":1 , "Nombre":1 , "ApellidoPaterno":1 , "ApellidoMaterno":1, "Militancias":1, "Sexo":1 , "FechaNacimiento":1 })
+    return get_db().proyectosdeley.find(
+        {},
+        {
+            "_id": id,
+            "Id": 1,
+            "Nombre": 1,
+            "ApellidoPaterno": 1,
+            "ApellidoMaterno": 1,
+            "Militancias": 1,
+            "Sexo": 1,
+            "FechaNacimiento": 1,
+        },
+    )
+
 
 def get_by_author(author_id):
     proyectos = []
-    for doc in get_db().proyectosdeley.aggregate([
+    for doc in get_db().proyectosdeley.aggregate(
+        [
             {
-                '$match': {
-                    'Votaciones.VotacionProyectoLey.Tipo.@Valor': '1',
-                    'Autores.ParlamentarioAutor.Diputado.Id':f'{author_id}'
+                "$match": {
+                    "Votaciones.VotacionProyectoLey.Tipo.@Valor": "1",
+                    "Autores.ParlamentarioAutor.Diputado.Id": f"{author_id}",
                 }
-            }, {
-                '$unwind': {
-                    'path': '$Votaciones.VotacionProyectoLey'
+            },
+            {"$unwind": {"path": "$Votaciones.VotacionProyectoLey"}},
+            {
+                "$match": {
+                    "Votaciones.VotacionProyectoLey.TipoVotacionProyectoLey.@Valor": "1"
                 }
-            }, {
-                '$match': {
-                    'Votaciones.VotacionProyectoLey.TipoVotacionProyectoLey.@Valor': '1'
+            },
+            {"$sort": {"Votaciones.VotacionProyectoLey.Fecha": -1}},
+            {
+                "$project": {
+                    "_id": 0,
+                    "Id": 1,
+                    "NumeroBoletin": 1,
+                    "Nombre": 1,
+                    "FechaIngreso": 1,
+                    "Votaciones.VotacionProyectoLey.Id": 1,
+                    "Votaciones.VotacionProyectoLey.Resultado": 1,
+                    "Votaciones.VotacionProyectoLey.Fecha": 1,
+                    "Materias": 1,
                 }
-            }, {
-                '$sort': {
-                    'Votaciones.VotacionProyectoLey.Fecha': -1
-                }
-            }, {
-                '$project': {
-                    '_id': 0, 
-                    'Id': 1, 
-                    'NumeroBoletin': 1, 
-                    'Nombre': 1, 
-                    'FechaIngreso': 1, 
-                    'Votaciones.VotacionProyectoLey.Id': 1, 
-                    'Votaciones.VotacionProyectoLey.Resultado': 1, 
-                    'Votaciones.VotacionProyectoLey.Fecha': 1,
-                    'Materias': 1
-                }
-            }
-        ]):
-            proyectos.append(doc )
+            },
+        ]
+    ):
+        proyectos.append(doc)
     return proyectos
